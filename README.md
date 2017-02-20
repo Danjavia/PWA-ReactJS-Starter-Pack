9118bc816bae498bc35d0487ae3a58158b64af85
Danny Viasus (Danjavia) <danjavia@gmail.com>
2017-02-19 22:26:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# starterpack
+
+Hello
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './App';
+import './index.css';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'http://my-api.graphql.com' }),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
