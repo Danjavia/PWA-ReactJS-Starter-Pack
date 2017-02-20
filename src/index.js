63f584b6ac203b4b69f66fe51104b294f2a351a2
Danny Viasus (Danjavia) <danjavia@gmail.com>
2017-02-20 15:20:33 -0500

    #Structure: Defining directory structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
-import App from './App';
+import App from './components/App/App';
 import './index.css';
 
 const client = new ApolloClient({
