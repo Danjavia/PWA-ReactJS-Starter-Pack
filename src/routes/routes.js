61c98c5e9daad2a38292d2e3b35a95fbdf4d7608
Danny Viasus (Danjavia) <danjavia@gmail.com>
2017-02-21 22:23:28 -0500

    #Update: update routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,20 +1,22 @@
 /**
  * External libraries
- * */
+ **/
 import React from 'react';
-import { IndexRoute, Route, Redirect } from 'react-router';
+import { IndexRoute, Route } from 'react-router';
 
+/**
+ * Internal Resources
+ **/
 import App from '../components/App/App';
 import Sample from '../components/Sample/Sample';
 import NotFound from '../components/NotFound/NotFound';
 
 /**
  * Router class definition.
- */
+ **/
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Sample} />
-    <Redirect from="/crm" to="/crm/opportunities/415199" />
     <Route path="*" component={NotFound} />
   </Route>
 );
