f5ce574fa4dd99f3962b6a1516f925b1458a47cf
Danny Viasus (Danjavia) <danjavia@gmail.com>
2017-02-21 22:19:31 -0500

    #Settings: Setting React-router within application

---
+++ src/components/Sample/Sample.js
@@ -0,0 +1,139 @@
+/**
+ * External Resources
+ **/
+import React, { Component } from 'react';
+
+/**
+ * Internal Resources
+ **/
+import './Sample.css';
+
+/**
+ * Sample class definition
+ **/
+class Sample extends Component {
+
+  /**
+   * constructor
+   **/
+  constructor() {
+    super();
+    this.state = {
+      files: [],
+      data: [], // data about them files, after processing
+    };
+    document.documentElement.ondragenter = e => e.preventDefault();
+    document.documentElement.ondragover = e => e.preventDefault();
+    document.documentElement.ondrop = e => {
+      e.preventDefault();
+      this.update(e.dataTransfer.files);
+    }
+  }
+
+  /**
+   * handleUploads
+   **/
+  handleUploads(e) {
+    this.update(e.target.files);
+  }
+
+  /**
+   * update
+   **/
+  update(moreFiles) {
+    const newFiles = Array.from(moreFiles);
+    if (!newFiles) {
+      return;
+    }
+    const files = this.state.files.concat(newFiles);
+    this.setState({files});
+    this.process(files);
+  }
+
+  /**
+   * process
+   **/
+  process(files) {
+    const data = this.state.data.slice();
+    files.forEach((f, idx) => {
+      if (!data[idx]) {
+        const i = new Image();
+        i.onload = () => {
+          data[idx] = i.naturalWidth + 'x' + i.naturalHeight;
+          this.setState({data});
+        };
+        i.src = window.URL.createObjectURL(f);
+      }
+    });
+  }
+
+  /**
+   * render
+   * @return {ReactElement} Markup
+   **/
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Upload me some images</h1>
+          <p>pst, you can just drop them anywhere</p>
+        </div>
+        <div className="Tool-in">
+          <Uploads onChange={this.handleUploads.bind(this)} />
+        </div>
+        <div className="Tool-out">
+          <Results files={this.state.files} data={this.state.data} />
+        </div>
+      </div>
+    );
+  }
+}
+
+/**
+ * Uploads component
+ **/
+const Uploads = ({onChange}) =>
+  <div>
+    <label htmlFor="files" className="Uploads-select">Select files...</label>
+    <input 
+      type="file" 
+      id="files" 
+      multiple 
+      accept="image/*" 
+      style={{display: 'none'}} 
+      onChange={onChange}
+    />
+  </div>;
+
+/**
+ * Results component
+ **/
+const Results = ({files, data}) => {
+  if (files.length === 0) {return <span/>;}
+  return (
+    <table className="Results-table">
+      <tbody>
+      <tr><th>Image</th><th>filename</th><th>bytes</th><th>mime</th><th>dimensions</th></tr>
+      {files.map((f, idx) => {
+        if (!f.type.startsWith('image/')) {
+          return null;
+        }
+        return (
+          <tr key={idx}>
+            <td><img alt={f.name} src={window.URL.createObjectURL(f)} height="60" /></td>
+            <td>{f.name}</td>
+            <td>{f.size}</td>
+            <td>{f.type}</td>
+            <td>{data[idx] || '...'}</td>
+          </tr>
+        );
+      })}
+      </tbody>
+    </table>
+  );
+};
+
+/**
+ * Export class
+ **/
+export default Sample;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+/**
+ * External Resources
+ **/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
-import App from './components/App/App';
+/**
+ * Internal Resources
+ **/
+import Routes from './routes/routes';
 
+/**
+ * `client`: Set apollo client connection
+ **/
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({ uri: 'http://my-api.graphql.com' }),
 });
 
+/**
+ * `Render`: Render application
+ **/
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Router history={hashHistory} routes={Routes} />
   </ApolloProvider>,
   document.getElementById('root')
 );
